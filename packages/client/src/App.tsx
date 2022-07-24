45bc3cbb6afd6e9707e8e31443136c95e87d6e3d
shrimpy <imxmwu@gmail.com>
2022-07-23 22:24:56 -0700

    test build 2

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!
+          Learn React!!
         </a>
         <br />
         {result}
