4e3313ad2ffdf11c139eebb43f8cd7a6cb11b787
shrimpy <imxmwu@gmail.com>
2023-05-14 22:00:05 -0700

    added logbal rate limiter

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,9 +1,24 @@
 import * as path from "path";
 import express from "express";
+import rateLimit from "express-rate-limit";
 import api from "./routes";
 
 const app = express();
 export default app;
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 10 // limit each IP to 10 requests per windowMs
+});
+
+//  apply to all requests
+// app.use(limiter);
+//  apply to api requests
+app.use("/api/", limiter);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
